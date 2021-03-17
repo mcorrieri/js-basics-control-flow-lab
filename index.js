64d5282fb5b8ea1b85c7e154f3eb04ea0c998ee3
Michael <Mlcorrieri@gmail.com>
2021-03-17 15:38:11 -0400

    commit tree cfc7f9f49e199f1161ba696271a9f4083a5186f0

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
+  // Write your code here!
+  if (ride < 400) return `This one is on me!`;
+  else if (ride > 2000) return `I will gladly take your thrity bucks.`;
+  else ride > 2500;
+  return `No can do`;
+}
+
+function ternaryCheckCity() {
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip() {
   // Write your code here!
 }
